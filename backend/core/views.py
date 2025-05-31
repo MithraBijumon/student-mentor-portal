5b338725751f13325d13c6fa48fd5c7a4adfb70a
Mithra Bijumon <mithrabijumon@gmail.com>
2025-05-31 12:47:18 +0530

    Fix: Import User model in UserViewSet

--- backend/core/views.py
+++ backend/core/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Post, Reply, DMMessage, Announcement
 from .serializers import PostSerializer, ReplySerializer, DMMessageSerializer, AnnouncementSerializer
+from django.contrib.auth.models import User
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
