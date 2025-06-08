ec345886c10168c1b9aa6b24fdb590e5ae6cae53
Mithra Bijumon <mithrabijumon@gmail.com>
2025-06-08 16:43:55 +0530

    Made first page login page

--- frontend/app/(tabs)/announcements.tsx
+++ frontend/app/(tabs)/announcements.tsx
@@ -1,3 +1,5 @@
+// app/(tabs)/AnnouncementsScreen.tsx
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 
 export default function AnnouncementsScreen() {
--- frontend/app/(tabs)/mentors.tsx
+++ frontend/app/(tabs)/mentors.tsx
@@ -1,3 +1,4 @@
+ //app/(tabs)/MentorsScreen.tsx
 import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList, StyleSheet, ActivityIndicator, TouchableOpacity } from 'react-native';
 import axios from 'axios';
