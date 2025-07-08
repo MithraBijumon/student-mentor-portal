0e9bc6e72469aa3773b63b535459696f0abd366a
aashnapulla <aashnapulla@gmail.com>
2025-07-09 01:05:04 +0530

    added announcements

--- frontend/app/env.ts
+++ frontend/app/env.ts
@@ -2,7 +2,7 @@
 const LOCAL = true; // set to false in production
 
 export const API_URL = LOCAL
-  ? "https://649fb13eada1.ngrok-free.app" // your local network IP + Django port
+  ? "https://192.168.0.101/8000" // your local network IP + Django port
   : "https://student-mentor-portal.onrender.com"; // deployed URL
 
 export const ENDPOINTS = {
